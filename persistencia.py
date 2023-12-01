ab0433c0d88dd97e3336190f2955d03e54ab22c0
cevilan <cevilan@gmail.com>
2023-12-01 13:24:48 +0100

    Update persistencia.py

--- persistencia.py
+++ persistencia.py
@@ -1,4 +1,11 @@
+"""
+Módulo persistencia
+"""
 def guardar_pedido(nombre, apellidos):
+    """
+    Function guardar_pedido
+    """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         file.write(nombre + " " + apellidos + "\n")
         file.close()
+        
