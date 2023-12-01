825742ad22b566d5d3e34db0d8ad9319a773b1af
cevilan <cevilan@gmail.com>
2023-12-01 14:18:40 +0100

    Actividad final

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """
 Módulo app
 """
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
 import persistencia
 
 app=Flask (__name__)
@@ -16,3 +16,18 @@ def pizza():
     apellidos=request.form.get("apellidos")
     persistencia.guardar_pedido(nombre,apellidos)
     return redirect("http://localhost/fullstack/solicita_pedido.html", code=302)
+
+@app.route("/checksize",methods=['POST'])
+
+def checksize():
+    """
+    Comprueba disponibilidad de un tamaño de pizza.
+    """
+    tamano=request.form.get("tamano")
+    
+    if tamano=='S':
+        mensaje="No disponible"
+    else:
+        mensaje="Disponible"
+    
+    return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'})
