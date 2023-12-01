5ffbf430148ff20e780caa225f33de1717f03637
cevilan <cevilan@gmail.com>
2023-12-01 13:23:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+"""
+Módulo app
+"""
 from flask import Flask, request, redirect
 import persistencia
 
 app=Flask (__name__)
 
-@app.route("/pizza",methods=['POST'])  
+@app.route("/pizza",methods=['POST'])
 
 def pizza():
-    nombre=request.form.get("nombre") 
-    apellidos=request.form.get("apellidos") 
-    
+    """
+    Function pizza
+    """
+    nombre=request.form.get("nombre")
+    apellidos=request.form.get("apellidos")
     persistencia.guardar_pedido(nombre,apellidos)
-    
     return redirect("http://localhost/fullstack/solicita_pedido.html", code=302)
